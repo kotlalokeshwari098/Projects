0aca09c7daff2833cfa676a1cdc0c52e3c8ca364
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-01-19 12:09:09 +0530

    generate HTML for order summary

--- Basic ECommerce/javascript-amazon-project-main/data/cart.js
+++ Basic ECommerce/javascript-amazon-project-main/data/cart.js
@@ -1,5 +1,13 @@
 // storing everything related to cart
-export const cart=[];
+export const cart=[{
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity:2
+},{
+    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity:1
+}];
+// this cart contains whatever we push by clicking add to cart
+
 
 export function addToCart(productId) {
     let matchingItem;
