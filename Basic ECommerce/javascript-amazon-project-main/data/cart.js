0035e0e4fb93607ce51a43e9770dd2a7872f58f0
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-01-19 13:27:49 +0530

    save cart in localstorage

--- Basic ECommerce/javascript-amazon-project-main/data/cart.js
+++ Basic ECommerce/javascript-amazon-project-main/data/cart.js
@@ -1,48 +1,59 @@
 // storing everything related to cart
-export let cart=[{
-    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2
-},{
-    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:1
-}];
+export let cart =JSON.parse(localStorage.getItem('cart'));
 // this cart contains whatever we push by clicking add to cart
+if(!cart){
+    cart=[{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    }, {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    }] 
+}
 
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+
+}
+// whenever cart updated it is saved in local storage
 
 export function addToCart(productId) {
-    let matchingItem;
+  let matchingItem;
 
-    let selectCart = document.querySelector(`.js-quantity-selector-${productId}`)
-    let cartValue = Number(selectCart.value)
+  let selectCart = document.querySelector(`.js-quantity-selector-${productId}`)
+  let cartValue = Number(selectCart.value)
 
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    })
-  
-    // check product is already in cart
-    if (matchingItem) {
-      matchingItem.quantity += cartValue;
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingItem = item;
     }
-    // if yes increase quantity
-    else {
-      cart.push({
-        productId,
-        quantity: cartValue
-      })
-    }
+  })
+
+  // check product is already in cart
+  if (matchingItem) {
+    matchingItem.quantity += cartValue;
   }
-
-
-export function removeFromCart(productId){
-    const newCart=[]
-
-    cart.forEach((cartItem)=>{
-      if(cartItem.productId !== productId){
-        newCart.push(cartItem);
-      }
+  // if yes increase quantity
+  else {
+    cart.push({
+      productId,
+      quantity: cartValue
     })
-
-    cart = newCart;
   }
+  saveToStorage();
+}
+
+
+export function removeFromCart(productId) {
+  const newCart = []
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  })
+
+  cart = newCart;
+  saveToStorage();
+}
