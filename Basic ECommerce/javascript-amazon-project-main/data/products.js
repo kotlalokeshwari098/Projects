0ac663c984d663dbb2557e23c52c3cf84e902d4d
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-02-15 22:50:57 +0530

    learn promises and convert code to promises

--- Basic ECommerce/javascript-amazon-project-main/data/products.js
+++ Basic ECommerce/javascript-amazon-project-main/data/products.js
@@ -84,6 +84,33 @@ console.log(objects.method());
 */
 // lets load products from backend not from that below one object
 export let products=[];
+
+
+export function loadProductsFetch(){
+   const promise = fetch('https://supersimplebackend.dev/products')
+   .then((response)=>{
+      return response.json();
+      // returing promise
+   }).then((productsData)=>{
+    productsData.map((productDetails)=>{
+      if(productDetails.type==='clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+     });
+  
+     console.log('load Products');
+     
+   })
+   return promise;
+}
+/*
+loadProductsFetch().then(()=>{
+  console.log('nextstep')
+});
+*/
+
+
 export function loadProducts(fun){
   
   const xhr=new XMLHttpRequest();
--- Basic ECommerce/javascript-amazon-project-main/scripts/checkout.js
+++ Basic ECommerce/javascript-amazon-project-main/scripts/checkout.js
@@ -3,10 +3,26 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 // import '../data/cart-class.js'; 
 // import '../data/backend-practice.js';
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+// as it return promise no need to make a new promise and return it, in line 11
+Promise.all([
+    loadProductsFetch(),
+    
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    })
 
+]).then((values)=>{
+    console.log(values)
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+})
+/*
 Promise.all([
     new Promise((resolve)=>{
         loadProducts(()=>{
